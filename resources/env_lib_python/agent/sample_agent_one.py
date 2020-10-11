92bfa55b6353c0239ffca499fb943c0c89d4088f
Dewmal Nilanka <dewmalnilanka@gmail.com>
2020-10-11 21:23:14 +0530

    Update init codes

--- resources/env_lib_python/agent/sample_agent_one.py
+++ resources/env_lib_python/agent/sample_agent_one.py
@@ -19,9 +19,11 @@ class AgentOne:
         await self.publish("AgentTwo", "Im Agent 1")
 
     async def accept_message(self, agent, message):
-        log.info("Inbox")
+        print("--------Agent 1 receiving messages-----------")
+        log.info("Inbox of agent 1")
         log.info(agent)
         log.info(message)
+        print("--------Agent 1 receiving messages-----------")
 
     async def stop(self, *args, **kwargs):
         log.info(f"{self.name} Stop")
@@ -34,6 +36,5 @@ class AgentOne:
         log.info(f"Kwargs = {kwargs}")
 
         while True:
-            await self.publish("AgentTwo", "Hi Agent 2")
-            await self.publish("AgentTwo", "Do you know me")
+            await self.publish("AgentTwo", "Hello AGENT 2")
             await asyncio.sleep(2)

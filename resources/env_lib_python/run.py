46ee4658734aa3993fd0487b757dd813f47d0144
Dewmal Nilanka <dewmalnilanka@gmail.com>
2021-01-31 07:56:09 +0530

    Update run file to has only required data

--- resources/env_lib_python/run.py
+++ resources/env_lib_python/run.py
@@ -42,14 +42,14 @@ class AgentWrapper:
         self._agent_.display = self.display
 
     async def start_agent(self):
-        if self._agent_.start:
+        if hasattr(self._agent_, "start"):
             try:
                 await self._agent_.start()
             except Exception as e:
                 log.exception(e)
 
     async def stop_agent(self):
-        if self._agent_.stop:
+        if hasattr(self._agent_, "stop"):
             try:
                 log.info(f"AGENT CLOSE REQUEST {self.id}")
                 await self._agent_.stop()
@@ -57,7 +57,7 @@ class AgentWrapper:
                 log.exception(e)
 
     async def execute_agent(self):
-        if self._agent_.execute:
+        if hasattr(self._agent_, "execute"):
             try:
                 await self._agent_.execute()
             except Exception as e:

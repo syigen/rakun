d14a27504af3682910a7f19844783c1b91c74a76
dewmal <dewmal@egreen.io>
2021-02-26 02:37:49 +0530

    init load balancing

--- internal/prepare/prepare.go
+++ internal/prepare/prepare.go
@@ -1,17 +1,21 @@
 package prepare
 
 import (
-	"github.com/ambelovsky/gosf"
 	gosocketio "github.com/ambelovsky/gosf-socketio"
-	"github.com/ambelovsky/gosf-socketio/transport"
 	"github.com/go-redis/redis/v8"
-	"log"
-	"net/http"
+)
+
+type AgentType string
+
+const (
+	DynamicAgent AgentType = "dynamic"
+	StaticAgent  AgentType = "static"
 )
 
 type Agent struct {
 	Name string
 	Code string
+	Type AgentType
 }
 
 type AgentList map[string]Agent
@@ -31,42 +35,4 @@ type Environment struct {
 	EnvPath          string
 	CommServerClient *redis.Client
 	DisplayServer    *gosocketio.Server
-}
-
-func (env *Environment) SetupCommServerClient() {
-	commUrl := env.Config.CommunicationUrl
-
-	env.CommServerClient = redis.NewClient(&redis.Options{
-		Addr:     commUrl,
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-}
-
-func (env *Environment) SetupDisplayServer() {
-	server := gosocketio.NewServer(transport.GetDefaultWebsocketTransport())
-	env.DisplayServer = server
-
-	server.On(gosocketio.OnConnection, func(c *gosocketio.Channel, args interface{}) {
-		//client id is unique
-		log.Println("New client connected, client id is ", c.Id())
-
-		//you can join clients to rooms
-		//c.Join("rakun")
-		c.Emit("test", gosf.NewSuccessMessage("Test"))
-
-		log.Println(c.IsAlive())
-
-	})
-	server.On("send", func(c *gosocketio.Channel, msg gosf.Message) string {
-		//send event to all in room
-		c.BroadcastTo("chat", "message", msg)
-		return "OK"
-	})
-
-	serveMux := http.NewServeMux()
-	serveMux.Handle("/socket.io/", server)
-	error := http.ListenAndServe(env.Config.DisplayServerUrl, serveMux)
-	log.Panic(error)
-
 }
